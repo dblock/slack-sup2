a58bede3bcad567030d4e1ea35458458d78c8568
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2023-04-29 11:19:09 -0400

    Fix: message user only once when opening home tab messages.

--- lib/events.rb
+++ lib/events.rb
@@ -16,7 +16,7 @@ SlackRubyBotServer::Events.configure do |config|
 
   config.on :event, 'event_callback', 'member_joined_channel' do |event|
     data = parse_team_event(event)
-    next unless data
+    next { ok: false } unless data
 
     Api::Middleware.logger.info "#{data.team.name}: bot joined ##{data.channel}."
     data.team.join_channel!(data.channel, data.inviter)
@@ -34,7 +34,7 @@ SlackRubyBotServer::Events.configure do |config|
 
   config.on :event, 'event_callback', 'member_left_channel' do |event|
     data = parse_team_event(event)
-    next unless data
+    next { ok: false } unless data
 
     Api::Middleware.logger.info "#{data.team.name}: bot left ##{data.channel}."
     data.team.leave_channel!(data.channel)
@@ -44,7 +44,14 @@ SlackRubyBotServer::Events.configure do |config|
 
   config.on :event, 'event_callback', 'app_home_opened' do |event|
     data = parse_user_event(event)
-    next unless data
+    next { ok: true } unless data && data.channel[0] == 'D'
+    next { ok: true } if Sup.where(conversation_id: data.channel).any?
+
+    channel = data.team.channels.where(channel_id: data.channel).first
+    next { ok: true } if channel
+
+    channel = data.team.channels.create!(channel_id: data.channel, enabled: false, sync: false, inviter_id: data.user)
+    channel.users.create!(user_id: data.user, sync: false, enabled: false, opted_in: false)
 
     text = [
       "Hi there! I'm your team's S'Up bot.",
--- spec/slack-sup/events/app_home_opened_spec.rb
+++ spec/slack-sup/events/app_home_opened_spec.rb
@@ -9,25 +9,48 @@ describe 'events/app_home_opened' do
     {
       type: 'app_home_opened',
       user: 'user_id',
-      channel: 'channel_id'
+      channel: 'DM'
     }
   end
 
   it 'welcomes user' do
     expect_any_instance_of(Slack::Web::Client).to receive(:chat_postMessage).with(
-      channel: 'channel_id', text: /Hi there! I'm your team's S'Up bot. Invite me to a channel/
+      channel: 'DM', text: /Hi there! I'm your team's S'Up bot. Invite me to a channel/
     )
 
-    post '/api/slack/event', event_envelope
-    expect(last_response.status).to eq 201
-    expect(JSON.parse(last_response.body)).to eq('ok' => true)
+    expect do
+      post '/api/slack/event', event_envelope
+      expect(last_response.status).to eq 201
+      expect(JSON.parse(last_response.body)).to eq('ok' => true)
+    end.to change(Channel, :count).by(1)
+
+    channel = team.channels.desc(:_id).limit(1).first
+    expect(channel.enabled).to be false
+    expect(channel.sync).to be false
+    expect(channel.channel_id).to eq 'DM'
+    expect(channel.users.count).to eq 1
+    user = channel.users.first
+    expect(user.user_id).to eq 'user_id'
+    expect(user.enabled).to be false
+    expect(user.opted_in).to be false
+    expect(user.sync).to be false
+  end
+
+  it 'welcomes user only once' do
+    expect_any_instance_of(Slack::Web::Client).to receive(:chat_postMessage).with(
+      channel: 'DM', text: /Hi there! I'm your team's S'Up bot. Invite me to a channel/
+    ).once
+
+    expect do
+      2.times { post '/api/slack/event', event_envelope }
+    end.to change(Channel, :count).by(1)
   end
 
   context 'with some channels' do
     let!(:channel) { Fabricate(:channel, team: team) }
     it 'welcomes user' do
       expect_any_instance_of(Slack::Web::Client).to receive(:chat_postMessage).with(
-        channel: 'channel_id', text: /Hi there! I'm your team's S'Up bot. I connect your teammates in 1 channel/
+        channel: 'DM', text: /Hi there! I'm your team's S'Up bot. I connect your teammates in 1 channel/
       )
 
       post '/api/slack/event', event_envelope
